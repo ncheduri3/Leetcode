e61c80aeae79a82983007d5c710a89ffe261b0e1
Namyata Cheduri <ncheduri3@gatech.edu>
2024-03-31 17:23:40 -0400

    Update FindMinimuminRotatedSortedArray.py

--- BinarySearch/FindMinimuminRotatedSortedArray.py
+++ BinarySearch/FindMinimuminRotatedSortedArray.py
@@ -19,3 +19,15 @@ class Solution:
                 right = mid - 1 
 
         return -5001
+
+        #better implementation
+        left, right = 0, len(nums)-1
+        
+        while left < right:
+            mid = (left + right) // 2
+            if nums[mid] > nums[right]:
+                left=mid + 1
+            else:
+                right = mid
+
+        return nums[left]
